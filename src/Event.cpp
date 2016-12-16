4feb3f7559889122562edc51f5fe140b73695ef0
Ali Viettry <aviettry96@gmail.com>
2016-12-16 00:53:20 -0800

    Completed findPlayer(name).  Added function to set the seeds.  Events contain four seeds. Also have function getSeed()

--- src/Event.cpp
+++ src/Event.cpp
@@ -8,15 +8,19 @@
 
 #include "Event.hpp"
 
+#define FIRST 1
+#define SECOND 2
+#define THIRD 3
+#define FOURTH 4
+
 Event::Event(std::string name) {
     this->name = name;
-    
 }
 
 Event::~Event() {}
 
 std::string Event::getName() {
-    return name;
+    return this->name;
 }
 
 int Event::getNumPlayers() {
@@ -32,4 +36,51 @@ bool Event::removePlayer(std::string name) {
     return false;
 }
 
-Player* findPlayer(std::string name) {}
+Player* findPlayer(std::string name) {
+    Player* found = 0;
+    auto itr = players.begin();
+    while( itr != players.end() ) {
+        found = *itr;
+        if( found->name == name ) {
+            return found;
+        }
+        itr++;
+    }
+    return 0;
+}
+
+bool Event::seedPlayer( int seed, std::string name ) {
+    if( seed == FIRST ) {
+        first_seed = findPlayer( name );
+        return true;
+    }
+    else if( seed == SECOND ) {
+        second_seed = findPlayer( name );
+        return true;
+    }
+    else if( seed == THIRD ) {
+        thrid_seed = findPlayer( name );
+        return true;
+    }
+    else if( seed == FOURTH ) {
+        fourth_seed = findPlayer( name );
+        return true;
+    }
+    return false;
+}
+
+Player* Event::getSeed( int seed ) {
+    if( seed == FIRST ) {
+        return first_seed;
+    }
+    else if( seed == SECOND ) {
+        return second_seed;
+    }
+    else if( seed == THIRD ) {
+        return thrid_seed;
+    }
+    else if( seed == FOURTH ) {
+        return fourth_seed;
+    }
+    return 0;
+}
--- src/Event.hpp
+++ src/Event.hpp
@@ -5,6 +5,16 @@
 //  Created by Martin Hung on 12/12/16.
 //
 //
+//TODO: Edit last modified field
+/** 
+ * Filename: Event.hpp
+ * Authors: Ali Viettry, Martin Hung
+ * Description: This is a container to store all player that compete in its
+ *              event. It will keep track of the number of players and provide a
+ *              ranking for which players are seeded.
+ * Date Created: 2016-12-12, Martin Hung
+ * Last Modified: 2016-12-16, Ali Viettry
+ */
 
 #ifndef Event_hpp
 #define Event_hpp
@@ -21,6 +31,10 @@ class Event {
 protected:
     std::string name;
     std::list<Player*> players;
+    Player* first_seed;    
+    Player* second_seed;    
+    Player* third_seed;    
+    Player* fourth_seed;;    
     
 public:
     Event(std::string name);
@@ -32,6 +46,8 @@ public:
     bool addPlayer(Player* player);
     bool removePlayer(std::string name);
     bool findPlayer(std::string name);
+    bool seedPlayer( int seed, std::string name );
+    Player* getSeed( int seed );
 };
 
 #endif /* Event_hpp */
